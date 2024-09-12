a374e1387e397fe88f1882cc00f85f6eb799511e
zeodao <zhou.dao2009@163.com>
2024-09-12 09:05:07 +0800

    finish Logical device pick

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "triangle.h"
+
+int main(int, char**) {
+  // setenv("DISPLAY", "192.168.21.216:10.0", 1);
+  // setenv("DISPLAY", "192.168.5.18:10.0", 1);
+  setenv("DISPLAY", ":0", 1);
+  TriangleApp app;
+  try {
+    app.Run();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
